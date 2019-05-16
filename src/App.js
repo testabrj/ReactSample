5c92fc245d49ad11e96b311a0f59d6b81ff223a4
Abrajeethan Alagarsamy <aalagarsamy@xactlycorp.com>
2019-05-16 14:33:54 +0530

    ReactSample: initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar'
+import TypoGraphy from '@material-ui/core/Typography'
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+        <AppBar color="primary" position="static">
+        <Toolbar>
+            <TypoGraphy variant="title"
+              color="inherit"
+            >
+              My header
+           </TypoGraphy>
+           <NavBar/>
+          </Toolbar>
+        </AppBar>
+
+      </div>
   );
 }
 
